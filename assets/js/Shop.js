9ce6cebd78b5998e414ab0bb786178daf914f1c1
VICode <105473822+VictorSilvaVS@users.noreply.github.com>
2025-03-16 20:51:13 -0300

    Update Shop.js
    number

--- assets/js/Shop.js
+++ assets/js/Shop.js
@@ -241,7 +241,7 @@ function removeFromCart(productId) {
 
 // Modificar a mensagem do WhatsApp
 function finishPurchase() {
-    const whatsappNumber = "558584298948";
+    const whatsappNumber = "555555555555";
     
     const now = new Date();
     const hour = now.getHours();
